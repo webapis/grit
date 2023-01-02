7db50cc970bde46cdc7f9c3d274e90c422bdacbb
webapis <60083568+webapis@users.noreply.github.com>
2023-01-02 12:02:38 +0500

    ssr for mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
-export default function App({ Component, pageProps }) {
 
-  return <Layout >
-<Component {...pageProps} />
+
+import { ThemeProvider } from "@mui/material";
+import { theme } from "../utils/theme";
+import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+const clientSideEmotionCache = createEmotionCache();
+export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache, }) {
+
+  return <CacheProvider value={emotionCache}>
+<ThemeProvider theme={theme}>
+<Layout >
+  <Component {...pageProps} />
   </Layout> 
+</ThemeProvider>
+</CacheProvider>
 }
