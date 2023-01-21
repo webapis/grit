87ea1b50e9d9d6b5b7fede798aaabf5d7aa6d0da
webapis <60083568+webapis@users.noreply.github.com>
2023-01-21 11:44:04 +0500

    Update unicodeReplacer.js

--- components/unicodeReplacer.js
+++ components/unicodeReplacer.js
@@ -12,6 +12,6 @@ export default function unicodeReplacer(array) {
     //     .replace(/%C3%BC/g, "ü")
     //     .replace(/%C4%B1/g, "ı")
     //     .replace(/%C4%9F/g, "ğ"))
-      return array.map(m => decodeURI( m))
+      return array.map(m => decodeURI( m).replace('_',' '))
 
 }
